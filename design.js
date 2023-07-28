cc194f14de37405225f579bdcb85f26c4d48556e
JYOTHIKANAVYASRICHEGONDI <85726266+jyothikachegondi@users.noreply.github.com>
2023-07-28 18:38:59 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,36 @@
+ReactDOM.render(<div>
+     <center> <h1><u>JAWAHARLAL NEHRU</u></h1>
+    <img src="https://images.librarywala.com/author/medium/5211578113907.jpg" />
+         <p><b>Born</b>:14 November 1889 ,Allahabad, North-Western Provinces, British India</p>
+         <p><b>Died</b>:27 May 1964 (aged 74) ,New Delhi, India</p>
+         <p><b>Resting place</b>: Shantivan</p>
+         <p><b>Spouse:</b>Kamala Kaul</p>
+         <p><b>Children:</b>Indira Gandhi</p>
+         <p><b>Political party</b>:Indian National Congress </p>​	
+     </center>
+    <h2>Early life and career (1889–1912)</h2>
+    <hr></hr>
+    <h2>Birth and family background</h2>
+     <p>Anand Bhawan the Nehru family home in Allahabad
+Jawaharlal Nehru was born on 14 November 1889 in Allahabad, British India. His father, Motilal Nehru, a self-made wealthy barrister of Kashmiri Pandit origin, served twice as president of the Indian National Congress, in 1919-20 and 1928-29. His mother, Swarup Rani Thussu, came from a Kashmiri Pandit family settled in Lahore.Jawaharlal was the eldest of three children.The elder of his two sisters, Vijaya Lakshmi, held political office and became president of the United Nations General Assembly.His youngest sister, Krishna Hutheesing, became a noted writer and biographer.</p>
+<h2>Childhood</h2>
+<p>Photograph of Nehru and his parents
+Jawaharlal with his parents Swarup Rani Nehru (left) and Motilal Nehru in 1894
+Nehru grew up in privilege at wealthy homes, including a palatial estate called the Anand Bhavan. His father, a leading socialite, enjoyed a luxurious and Westernised lifestyle, which the younger Nehru was encouraged to adopt. Nehru later described his childhood as "sheltered and uneventful". His father had him educated at home by English governesses and private tutors. Influenced by Irish-French theosophist Ferdinand T. Brooks, Nehru became interested in science, literature, and theosophy. </p>
+<h2>Youth</h2>
+<p>Nehru and his family left for England in May 1905 to seek his admission at Harrow, a boarding school in London.He was left in London to learn Latin and prepare for the entrance examinations. He began attending in the fall, and wrote to his father that "Harrow agrees with me quite well". He was nicknamed "Joe", and did well academically and socially. His headmaster wrote to his father in March 1906 that Nehru was "...doing remarkably well in every way, being again head of his form, and winning golden opinions from his masters for his industry and ability". A school report from October 1906 states: "...Progress good — has brains". Nehru's younger contemporary, Harold Alexander, remembered him as an "average popular boy".Nehru was a member of the Harrow School Corps, and excelled in cadet activities like shooting and riding.</p>
+<center><img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Nehru_at_Harrow.png" /></center>
+<h2>Graduation</h2>
+<p>Nehru went to Trinity College, Cambridge, in October 1907 and graduated with an honours degree in natural science in 1910.[33] At Cambridge, he read widely on politics, economics, history and literature. He traced the beginning of his interest in socialism at Cambridge to the Fabian Society.[34] In 1910, Nehru moved to London and started studying law at the Inner Temple Inn. He was called to the Bar in 1912.</p>
+<h2>Advocate practice</h2>
+<center><img src="https://upload.wikimedia.org/wikipedia/commons/3/34/Nehru_barrister.png" /></center>
+<p>Photograph if Nehru in his barrister's attire
+Jawaharlal Nehru, Barrister-at-Law
+After returning to India in August 1912, Nehru enrolled as an advocate of the Allahabad High Court and practised in his father's chambers. However, he did not enjoy the practice of law and found the work intellectually unsatisfying. Nationalist politics replaced his legal practice after 1920. He would not work as a barrister again until the INA trials in 1945-46.</p>
+<h2>Nationalist movement (1912–1939)</h2>
+<hr></hr>
+<h2>Congress and civil rights: 1912–1913</h2>
+<p>Nehru developed an interest in Indian politics during his time in Britain. In December 1912, he attended the annual session of the Congress at Patna. The Congress was then a party of moderates and elites, and Nehru described the meeting as "very much an English-knowing upper-class affair".He doubted the effectiveness of the Congress, but agreed to collect funds for the Indian civil rights movement led by Mahatma Gandhi in South Africa.He later campaigned against indentured labour and discriminations faced by Indians in the British colonies.</p></div>
+,
+document.getElementById("content")
+);
